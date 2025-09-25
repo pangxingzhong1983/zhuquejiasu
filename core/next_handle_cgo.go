86c3c33645501fb4e64d3ca030682ea722f945ac
pangxingzhong <pangxingzhong@gmail.com>
2025-09-25 13:14:10 +0800

    fix: exclude android from cgo fallback

--- core/next_handle_cgo.go
+++ core/next_handle_cgo.go
@@ -1,7 +1,6 @@
-//go:build cgo
+//go:build cgo && !android
 
 package main
 
-// Fallback nextHandle for CGO builds where server.go is excluded
+// Fallback nextHandle for CGO builds where server.go is excluded (non-Android)
 func nextHandle(action *Action, result func(data interface{})) bool { return false }
-
